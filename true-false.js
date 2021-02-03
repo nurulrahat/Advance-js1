26f8f2c40c085fd549fe28c24f3f02addae7f33e
Nurul Rahat <nurulrahat02@gmail.com>
2021-02-03 12:48:38 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,12 @@
+const noString = ""; //false
+// const empString = " ";//true
+// const noNumber = NaN;//false
+// const notdefined = undefined;//false
+// const noValueassign;//false as undefined
+// const nullValue = null;//false
+// const arrayName = []; //true;
+// const objectName = {};//true
+if(noString)
+console.log("It's True");
+else
+console.log("It's false")
